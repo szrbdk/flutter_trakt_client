36e0a824826670e335e13627efc43dece5c35436
Sezer Budak <szrbdk@gmail.com>
2021-03-28 22:27:44 +0300

    some enum files renamed

--- lib/trakt_client.dart
+++ lib/trakt_client.dart
@@ -5,8 +5,8 @@ export 'package:trakt_client/src/trakt_service.dart';
 export 'package:trakt_client/src/trakt_client.dart';
 
 // ENUMS
-export 'package:trakt_client/src/enum/comment_type.dart';
-export 'package:trakt_client/src/enum/extended_info.dart';
+export 'package:trakt_client/src/enum/comment_enum.dart';
+export 'package:trakt_client/src/enum/extended_info_enum.dart';
 export 'package:trakt_client/src/enum/grant_type.dart';
 export 'package:trakt_client/src/enum/simple_type.dart';
 export 'package:trakt_client/src/enum/type.dart';
